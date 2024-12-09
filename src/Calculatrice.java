5e860346ace5c7c88ba78d06b4a3366395a7a780
kinanix7 <kinanix7279x@gmail.com>
2024-12-09 15:25:10 +0100

    ajouter menu principal

---
+++ src/Calculatrice.java
@@ -0,0 +1,11 @@
+
+public class Calculatrice {
+
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double soustraction(double num1, double num2){
+        return num1 - num2 ;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double num1,num2;
+        int operator ;
+
+        System.out.println(" Menu principal :\n");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Soustraction");
+        System.out.println("3 - Multiplication");
+        System.out.println("4 - Division");
+        System.out.println("5 - Puissance");
+        System.out.println("6 - Racine carrée");
+        System.out.println("7 - Factorielle");
+        System.out.println("8 - Quitter");
+
+        System.out.print("Entre votre choix :");
+        Scanner sc = new Scanner(System.in);
+        operator = sc.nextInt();
+        System.out.print("Enter first number : ");
+        num1 = sc.nextInt();
+        System.out.print("Enter second number : ");
+        num2 = sc.nextInt();
+
     }
+
+
 }
