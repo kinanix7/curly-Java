a3b204b017f641ff431b19c3151445be923acb05
kinanix7 <kinanix7279x@gmail.com>
2024-12-09 15:33:01 +0100

    ajouter func multiplication et division

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -5,7 +5,18 @@ public class Calculatrice {
         return num1 + num2;
     }
 
+
     public static double soustraction(double num1, double num2){
         return num1 - num2 ;
     }
+
+
+    public static double multiplication(double num1 ,double num2){
+        return num1 * num2 ;
+    }
+
+    public static double division(double num1 , double num2){
+        return num1 / num2 ;
+    }
+
 }
