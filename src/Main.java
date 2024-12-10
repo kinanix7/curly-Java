1368839655f4fe91a332adf45a25dc3856dc08bc
kinanix7 <kinanix7279x@gmail.com>
2024-12-10 11:21:44 +0100

    Fix code

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,13 @@ public class Main {
             System.out.println("3 - Multiplication");
             System.out.println("4 - Division");
             System.out.println("5 - Puissance");
-            System.out.println("6 - Racine carrée");
+            System.out.println("6 - Racine carree");
             System.out.println("7 - Factorielle");
             System.out.println("8 - Quitter");
             System.out.print("Entre votre choix : ");
             operator = sc.nextInt();
 
-            // Exit
+            // Esc
             if (operator == 8) {
                 System.out.println("Au revoir !");
                 break;
