3f7b0fd038ffb478fe83e0857851e446453d0fdc
kinanix7 <kinanix7279x@gmail.com>
2024-12-10 11:09:11 +0100

    Fix code

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -16,6 +16,9 @@ public class Calculatrice {
     }
 
     public static double division(double num1 , double num2){
+        if (num2 == 0){
+            System.out.println("Erreur !");
+        }
         return num1 / num2 ;
     }
 
@@ -23,11 +26,14 @@ public class Calculatrice {
         return Math.pow(num1,num2);
     }
 
-    public static double factorielle(int num1){
-        return num1 * factorielle(num1 - 1);
-    }
-
     public static double racinecarree(int num1){
         return Math.sqrt(num1);
+    }
+
+    public static int factorielle(int num1){
+        if (num1 <= 1){
+            return 1;
+        }
+        return num1 * factorielle(num1 - 1);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,72 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double num1,num2;
-        int operator ;
-
-        System.out.println(" Menu principal :\n");
-        System.out.println("1 - Addition");
-        System.out.println("2 - Soustraction");
-        System.out.println("3 - Multiplication");
-        System.out.println("4 - Division");
-        System.out.println("5 - Puissance");
-        System.out.println("6 - Racine carrée");
-        System.out.println("7 - Factorielle");
-        System.out.println("8 - Quitter");
-
-        System.out.print("Entre votre choix :");
+        double num1, num2;
+        int operator;
         Scanner sc = new Scanner(System.in);
-        operator = sc.nextInt();
-        System.out.print("Enter first number : ");
-        num1 = sc.nextInt();
-        System.out.print("Enter second number : ");
-        num2 = sc.nextInt();
 
+        while (true) {
+            System.out.println("\nMenu principal :\n");
+            System.out.println("1 - Addition");
+            System.out.println("2 - Soustraction");
+            System.out.println("3 - Multiplication");
+            System.out.println("4 - Division");
+            System.out.println("5 - Puissance");
+            System.out.println("6 - Racine carrée");
+            System.out.println("7 - Factorielle");
+            System.out.println("8 - Quitter");
+            System.out.print("Entre votre choix : ");
+            operator = sc.nextInt();
+
+            // Exit
+            if (operator == 8) {
+                System.out.println("Au revoir !");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            num1 = sc.nextDouble();
+
+
+            if (operator != 6 && operator != 7) {
+                System.out.print("Enter second number: ");
+                num2 = sc.nextDouble();
+            } else {
+                num2 = 0;
+            }
+
+            switch (operator) {
+                case 1:
+                    System.out.println("Addition : " + Calculatrice.addition(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Soustraction : " + Calculatrice.soustraction(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Multiplication : " + Calculatrice.multiplication(num1, num2));
+                    break;
+                case 4:
+                    System.out.println("Division : " + Calculatrice.division(num1, num2));
+                    break;
+                case 5:
+                    System.out.println("Puissance : " + Calculatrice.puissance(num1, num2));
+                    break;
+                case 6:
+                    System.out.println("Racinecarree : " + Calculatrice.racinecarree((int) num1));
+
+                    break;
+                case 7:
+                    if (num1 < 0) {
+                        System.out.println("Erreur : Factorielle d'un nombre negatif ");
+                    } else {
+                        System.out.println("Factorielle : " + Calculatrice.factorielle((int) num1));
+                    }
+                    break;
+                default:
+                    System.out.println("Option invalide");
+            }
+        }
+
+        sc.close();
     }
-
-
 }
